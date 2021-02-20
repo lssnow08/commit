1dff6ca70fa97428f3125bf682aae62a520cc88a
lssonw08 <3047127189@qq.com>
2021-02-20 22:03:37 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+let array = [1,2,3,4,5,6,7].length;
+console.log(array);
+
+
+
+let ob = {"name":"long","age":"18"};
+let obj = Object.keys(ob);
+console.log(ob.name,ob.age);
+console.log(obj.length);
